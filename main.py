295031912f2a0a05545687793d1e1291312c4c7b
unknown <lukasz.harkot22@gmail.com>
2023-05-16 08:55:58 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/students")
+def get_students():
+    return{
+        "students: [
+            {
+                id:0
+            }
+        ]"
+    }
